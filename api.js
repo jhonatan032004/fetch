d557d24e3ece5d965928fbd9b0d5cdf58ef027ea
jhonatan <jpalacio172@misena.edu.co>
2022-09-14 17:29:43 -0500

    primer commit

---
+++ api.js
@@ -0,0 +1,48 @@
+// fetch('datos.txt')
+// .then(function(response){ 
+//     return response.text();
+// }) //Que clase de informacion estoy solicitando
+// .then(function (data){ 
+//     console.log(data);
+// }) //Extrae los datos de consumo
+// .catch(function(error){
+//     console.log(`Hemos tenido un error ${error}`)
+// })
+
+// fetch('datos.txt')
+// .then((response)=>{return response.text()})
+// .then((data)=>{console.log(data)})
+// .catch((error)=>{console.log(`Hemos tenido un error ${error}`)})
+
+// let url = 'datos.txt';
+// fetch(url)
+// .then(response=>response.text())
+// .then(data=>{
+//     let info = document.getElementById('dato');
+//     info.innerHTML = data;
+// })
+// .catch(error=>console.log(`Hemos tenido un error ${error.message()}`))
+
+let url = 'datos.json';
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        let imp = document.getElementById('dato')
+        let mensaje = ""
+            for (let index = 0; index < data.length; index++) {
+            mensaje = mensaje + 
+            data[index].nombre +
+            " " + data[index].apellido +
+            "<b><br> Telefono: </b>" +
+            data[index].telefono +
+            "<b><br> Cedula: </b>" + data[0].cedula + "<br><br>" 
+            }
+            imp.innerHTML = mensaje;
+
+        console.log(data)
+    })
+
+
+
+
+
---
+++ comilla.js
@@ -0,0 +1,4 @@
+console.log("hello world");
+let nombre = "carlos";
+console.log("hello world "+ nombre);
+console.log(`hello world ${nombre} welcome`);
